befb98c0dd70083a1c412b3be4eb5754444ce0e8
Kusefiru <kusefiru@outlook.com>
2026-03-04 21:47:48 +0100

    Add toast notification on API errors, improve some return values

--- src/lib/opensubsonic/api.js
+++ src/lib/opensubsonic/api.js
@@ -3,6 +3,7 @@
 import md5 from "md5";
 import { get } from 'svelte/store';
 import { session } from "$lib/stores/auth.svelte.js";
+import toast from "svelte-french-toast";
 
 // Utility methods
 
@@ -29,6 +30,22 @@ function buildUrl(endpoint, extraParams = {}) {
     return url.toString();
 }
 
+async function ask(endpoint, extraParams = {}) {
+    const response = await fetch(buildUrl(endpoint, extraParams));
+    if (!response.ok) {
+        toast.error(`${response.status}: ${response.statusText}`);
+        return null;
+    }
+    const reply = await response.json();
+    const data = reply["subsonic-response"];
+
+    if (data.status !== 'ok') {
+        toast.error(`In ${endpoint}: ${data.error.message} (error ${data.error.code})`);
+        return null;
+    }
+    return data;
+}
+
 // API implementation
 
 export function download(id) {
@@ -36,45 +53,33 @@ export function download(id) {
 }
 
 export async function getAlbum(id) {
-    const response = await fetch(buildUrl('getAlbum.view', { id }));
-    const data = await response.json();
-
-    return data['subsonic-response'].album;
+    const reply = await ask('getAlbum.view', { id });
+    return reply?.album || null;
 }
 
 export async function getAlbumList(type, size) {
-    const response = await fetch(buildUrl('getAlbumList.view', { type, size }));
-    const data = await response.json();
-
-    return data['subsonic-response'].albumList;
+    const reply = await ask('getAlbumList.view', { type, size });
+    return reply?.albumList?.album || [];
 }
 
 export async function getAlbumList2(type, options = {}) {
-    const response = await fetch(buildUrl('getAlbumList2.view', { type, ...options }));
-    const data = await response.json();
-
-    return data['subsonic-response'].albumList2?.album || [];
+    const reply = await ask('getAlbumList2.view', { type, ...options });
+    return reply?.albumList2?.album || [];
 }
 
 export async function getArtist(id) {
-    const response = await fetch(buildUrl('getArtist.view', { id }));
-    const data = await response.json();
-
-    return data['subsonic-response'].artist;
+    const reply = await ask('getArtist.view', { id });
+    return reply?.artist || null;
 }
 
 export async function getArtists() {
-    const response = await fetch(buildUrl('getArtists.view'));
-    const data = await response.json();
-
-    return data['subsonic-response'].artists;
+    const reply = await ask('getArtists.view', { id: 'aaaa' });
+    return reply?.artists || [];
 }
 
 export async function getArtistInfo2(id, options = {}) {
-    const response = await fetch(buildUrl('getArtistInfo2.view', { id, ...options }));
-    const data = await response.json();
-
-    return data['subsonic-response'].artistInfo2;
+    const reply = await ask('getArtistInfo2.view', { id, ...options });
+    return reply?.artistInfo2;
 }
 
 export async function getAvatar(username) {
@@ -84,73 +89,53 @@ export async function getAvatar(username) {
 }
 
 export async function getIndexes(musicFolderId, isModifiedSince) {
-    const response = await fetch(buildUrl('getIndexes.view', { musicFolderId, isModifiedSince }));
-    const data = await response.json();
-
-    return data['subsonic-response'].indexes;
+    const reply = await ask('getIndexes.view', { musicFolderId, isModifiedSince });
+    return reply?.indexes;
 }
 
 export async function getMusicDirectory(id) {
-    const response = await fetch(buildUrl('getMusicDirectory.view', { id }));
-    const data = await response.json();
-
-    return data['subsonic-response'].directory;
+    const reply = await ask('getMusicDirectory.view', { id });
+    return reply?.directory;
 }
 
 export async function getMusicFolders() {
-    const response = await fetch(buildUrl('getMusicFolders.view'));
-    const data = await response.json();
-
-    return data['subsonic-response'].musicFolders.musicFolder;
+    const reply = await ask('getMusicFolders.view');
+    return reply?.musicFolders?.musicFolder || [];
 }
 
 export async function getPlaylist(id) {
-    const response = await fetch(buildUrl('getPlaylist.view', { id }));
-    const data = await response.json();
-
-    return data['subsonic-response'].playlist;
+    const reply = await ask('getPlaylist.view', { id });
+    return reply?.playlist || null;
 }
 
 export async function getPlaylists() {
-    const response = await fetch(buildUrl('getPlaylists.view'));
-    const data = await response.json();
-
-    return data['subsonic-response'].playlists?.playlist || [];
+    const reply = await ask('getPlaylists.view');
+    return reply?.playlists?.playlist || [];
 }
 
 export async function getPlayQueue() {
-    const response = await fetch(buildUrl('getPlayQueue.view'));
-    const data = await response.json();
-
-    return data['subsonic-response'].playQueue;
+    const reply = await ask('getPlayQueue.view');
+    return reply?.playQueue;
 }
 
 export async function getSong(id) {
-    const response = await fetch(buildUrl('getSong.view', { id }));
-    const data = await response.json();
-
-    return data['subsonic-response'].song;
+    const reply = await ask('getSong.view', { id });
+    return reply?.song;
 }
 
 export async function getStarred() {
-    const response = await fetch(buildUrl('getStarred.view'));
-    const data = await response.json();
-
-    return data['subsonic-response'].starred;
+    const reply = await ask('getStarred.view');
+    return reply?.starred || {};
 }
 
 export async function getTopSongs(name) {
-    const response = await fetch(buildUrl('getTopSongs.view', { artist: name, count: 10 }));
-    const data = await response.json();
-
-    return data['subsonic-response'].topSongs.song;
+    const reply = await ask('getTopSongs.view', { artist: name, count: 10 });
+    return reply?.topSongs?.song || [];
 }
 
 export async function getUser(name) {
-    const response = await fetch(buildUrl('getUser.view', { username: name }));
-    const data = await response.json();
-
-    return data['subsonic-response'].user;
+    const reply = await ask('getUser.view', { username: name });
+    return reply?.user;
 }
 
 export function getCoverArtUrl(coverArtId, size) {
@@ -163,35 +148,31 @@ export function getStreamUrl(id) {
 }
 
 export async function ping() {
-    const response = await fetch(buildUrl('ping.view'));
-    const data = await response.json();
-
-    return data['subsonic-response'].status;
+    const reply = await ask('ping.view');
+    return reply?.status;
 }
 
 export async function savePlayQueue(trackIds, current, position) {
-    const response = await fetch(buildUrl('savePlayQueue.view', { id: trackIds, current, position }));
-    const data = await response.json();
+    const reply = await ask('savePlayQueue.view', { id: trackIds, current, position });
+    return reply?.status;
 }
 
 export async function scrobble(id, time, submission = false) {
-    const response = await fetch(buildUrl('scrobble.view', { id, time, submission }));
-    const data = await response.json();
+    const reply = await ask('scrobble.view', { id, time, submission });
+    return reply?.status;
 }
 
 export async function search3(query) {
-    const response = await fetch(buildUrl('search3.view', { query, songCount: 10 }));
-    const data = await response.json();
-
-    return data['subsonic-response'].searchResult3;
+    const reply = await ask('search3.view', { query, songCount: 10 });
+    return reply?.searchResult3;
 }
 
 export async function star(id) {
-    const response = await fetch(buildUrl('star.view', { id }));
-    const data = await response.json();
+    const reply = await ask('star.view', { id });
+    return reply?.status;
 }
 
 export async function unstar(id) {
-    const response = await fetch(buildUrl('unstar.view', { id }));
-    const data = await response.json();
+    const reply = await ask('unstar.view', { id });
+    return reply?.status;
 }
