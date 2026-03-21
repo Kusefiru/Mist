2200adc4c907ffb4f251fc95f7db695536186b33
Kusefiru <kusefiru@outlook.com>
2026-03-21 21:49:07 +0100

    Change headers to scale a bit based on breakpoints

--- src/lib/components/album/AlbumHeader.svelte
+++ src/lib/components/album/AlbumHeader.svelte
@@ -18,7 +18,7 @@
     <div
         class="absolute inset-0 z-5 bg-surface-10/50 shadow-[inset_0_0_48px_oklch(from_var(--color-surface-30)_l_c_h_/_1.0)] dark:bg-surface-50/50"
     ></div>
-    <div class="relative z-10 mr-4 flex size-64 items-center justify-center">
+    <div class="relative z-10 mr-4 flex size-44 items-center justify-center sm:size-56 lg:size-64">
         <FadeImage
             src={coverArt}
             alt={album.name}
@@ -32,14 +32,16 @@
         class="group relative flex items-end overflow-hidden rounded-[0.4rem] p-4 shadow-md shadow-surface-30 select-none"
     >
         {@render cardCover(cache.getCoverArt(album.coverArtId))}
-        <div class="relative z-10 flex flex-col gap-1 pr-12 text-ink-700">
+        <div class="relative z-10 flex flex-col pr-12 text-ink-700 lg:gap-1">
             <h4 class="text-sm font-bold uppercase">{album.typeStr}</h4>
             <div class="flex flex-row items-baseline gap-2 text-ink-800">
-                <h2 class="text-3xl font-bold break-words whitespace-normal">{album.name}</h2>
-                <h2 class="text-3xl font-bold text-ink-700 italic">{album.version}</h2>
-                <Star size={"1.75rem"} />
+                <h2 class="text-2xl font-bold break-words whitespace-normal lg:text-3xl">
+                    {album.name}
+                </h2>
+                <h2 class="text-2xl font-bold text-ink-700 italic lg:text-3xl">{album.version}</h2>
+                <Star size={'1.75rem'} />
             </div>
-            <h3 class="text-2xl">
+            <h3 class="text-xl lg:text-2xl">
                 <FormattedArtists text={album.artistsStr} artistMap={album.artistIds} />
             </h3>
             <h3 class="truncate text-lg font-semibold">
@@ -54,6 +56,8 @@
     <div
         class="relative flex animate-pulse items-end overflow-hidden rounded-[0.4rem] p-4 shadow-md shadow-surface-30"
     >
-        <div class="mr-4 aspect-square size-64 overflow-hidden rounded bg-surface-30"></div>
+        <div
+            class="mr-4 aspect-square size-44 overflow-hidden rounded bg-surface-30 sm:size-56 lg:size-64"
+        ></div>
     </div>
 {/if}
--- src/lib/components/playlist/PlaylistHeader.svelte
+++ src/lib/components/playlist/PlaylistHeader.svelte
@@ -16,7 +16,7 @@
     <div
         class="absolute inset-0 z-5 bg-surface-10/50 shadow-[inset_0_0_48px_oklch(from_var(--color-surface-30)_l_c_h_/_1.0)] dark:bg-surface-50/50"
     ></div>
-    <div class="relative z-10 mr-4 flex size-64 items-center justify-center">
+    <div class="relative z-10 mr-4 flex size-44 items-center justify-center sm:size-56 lg:size-64">
         <FadeImage
             src={coverArt}
             alt={playlist.name}
@@ -30,9 +30,9 @@
         class="group relative flex items-end overflow-hidden rounded-[0.4rem] p-4 shadow-md shadow-surface-30 select-none"
     >
         {@render cardCover(cache.getCoverArt(playlist.coverArtId))}
-        <div class="relative z-10 flex flex-col gap-1 pr-12 text-ink-700">
+        <div class="relative z-10 flex flex-col pr-12 text-ink-700 lg:gap-1">
             <h4 class="text-sm font-bold uppercase">Playlist</h4>
-            <h2 class="text-3xl font-bold break-words whitespace-normal text-ink-800">
+            <h2 class="text-2xl font-bold break-words whitespace-normal text-ink-800 lg:text-3xl">
                 {playlist.name}
             </h2>
             <h3 class="truncate text-lg font-semibold">
@@ -48,6 +48,8 @@
     <div
         class="relative flex animate-pulse items-end overflow-hidden rounded-[0.4rem] p-4 shadow-md shadow-surface-30"
     >
-        <div class="mr-4 aspect-square size-64 overflow-hidden rounded bg-surface-30"></div>
+        <div
+            class="mr-4 aspect-square size-44 overflow-hidden rounded bg-surface-30 sm:size-56 lg:size-64"
+        ></div>
     </div>
 {/if}
