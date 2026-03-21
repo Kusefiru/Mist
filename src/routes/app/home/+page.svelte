a15fd8466b09d09c2dbaff9a9281067aea668c0f
Kusefiru <kusefiru@outlook.com>
2026-03-21 15:13:21 +0100

    Use more rows for large cards on home page for smaller screens

--- src/routes/app/home/+page.svelte
+++ src/routes/app/home/+page.svelte
@@ -3,7 +3,7 @@
     import AlbumRow from '$lib/components/album/AlbumRow.svelte';
     import ArtistCardLarge from '$lib/components/artist/ArtistCardLarge.svelte';
     import { cache } from '$lib/stores/cache.svelte';
-    import { shuffleArray } from "$lib/utils/shuffle";
+    import { shuffleArray } from '$lib/utils/shuffle';
 
     async function loadHomepage() {
         const recent = await cache.getAlbumList('recent', { size: 4 });
@@ -19,10 +19,12 @@
 
 {#await homepagePromise then { newest, recent, random, artist }}
     <div class="flex flex-col gap-4 overflow-x-clip overflow-y-auto px-8 pt-2 pb-12 lg:gap-6">
-        <div class="relative flex w-full flex-col gap-4">
+        <div class="@container relative flex w-full flex-col gap-4">
             <h2 class="text-2xl font-bold text-ink-800">Welcome, {cache.user.id}</h2>
             {#if recent.length > 0}
-                <div class="grid h-[120px] grid-cols-4 grid-rows-1 gap-3">
+                <div
+                    class="grid h-[12.5rem] grid-cols-2 grid-rows-2 gap-3 @header-4col:h-[7.5rem] @header-4col:grid-cols-4 @header-4col:grid-rows-1"
+                >
                     {#each recent as album}
                         <a href="/app/album/{album.id}">
                             <AlbumCardLarge {album} />
@@ -33,9 +35,11 @@
         </div>
         <AlbumRow title="New albums" albums={newest} />
         <AlbumRow title="Random albums" albums={random} />
-        <div class="relative flex w-full flex-col gap-4">
+        <div class="@container relative flex w-full flex-col gap-4">
             <h2 class="text-2xl font-bold text-ink-800">Random artists</h2>
-            <div class="grid h-[120px] grid-cols-4 grid-rows-1 gap-3">
+            <div
+                class="grid h-[12.5rem] grid-cols-2 grid-rows-2 gap-3 @header-4col:h-[7.5rem] @header-4col:grid-cols-4 @header-4col:grid-rows-1"
+            >
                 {#each artist as artist}
                     <a href="/app/artist/{artist.id}">
                         <ArtistCardLarge {artist} />
