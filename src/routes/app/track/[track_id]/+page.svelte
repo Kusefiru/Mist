53c1f401245513a624bbd5f415fe53a42e854043
Kusefiru <kusefiru@outlook.com>
2026-05-31 23:19:25 +0200

    Add track-id and track radio pages

---
+++ src/routes/app/track/[track_id]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { cache } from '$lib/stores/cache.svelte';
+    import { goto } from "$app/navigation";
+
+    let { params } = $props();
+
+    $effect(() => {
+        /* Just redirect, this page does nothing */
+        const track = cache.tracks.get(params.track_id);
+        if (track) goto(`/app/album/${track.albumId}`);
+        goto(`/app/home`);
+    })
+</script>
