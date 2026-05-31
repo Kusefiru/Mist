3ca258ac34597a5d3d45578204d6e139f3a2377e
Kusefiru <kusefiru@outlook.com>
2026-05-31 21:58:14 +0200

    Change icon when hovering liked track Heart

--- src/lib/components/ui/Star.svelte
+++ src/lib/components/ui/Star.svelte
@@ -1,10 +1,13 @@
 <script>
-    import { Heart } from 'phosphor-svelte';
+    import { Heart, HeartBreak } from 'phosphor-svelte';
     import { cache } from '$lib/stores/cache.svelte.js';
     import { star, unstar } from '$lib/opensubsonic/api';
 
     let { trackId, size = 20, hidden = false } = $props();
+
     const starred = $derived(cache.stars.has(trackId));
+
+    let hovered = $state(false);
 
     function toggleStar() {
         if (!starred) {
@@ -22,7 +25,13 @@
         class="flex-shrink-0 cursor-pointer transition-colors hover:text-primary-20"
         class:text-primary-10={starred}
         onclick={toggleStar}
+        onmouseenter={() => (hovered = true)}
+        onmouseleave={() => (hovered = false)}
     >
-        <Heart {size} weight={starred ? 'fill' : 'bold'} />
+        {#if starred && hovered}
+            <HeartBreak {size} weight='fill' />
+        {:else}
+            <Heart {size} weight={starred ? 'fill' : 'bold'} />
+        {/if}
     </button>
 {/if}
