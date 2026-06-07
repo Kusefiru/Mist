f892b69c36465e0c2cea81088f6999f964c8e6f0
Kusefiru <kusefiru@outlook.com>
2026-06-07 23:06:02 +0200

    Add current version to settings page

--- src/routes/app/settings/+page.svelte
+++ src/routes/app/settings/+page.svelte
@@ -1,5 +1,10 @@
 <script>
-    import { Palette } from 'phosphor-svelte';
+    import { Info, Palette } from 'phosphor-svelte';
+    import {
+        PUBLIC_VERSION_COMMIT,
+        PUBLIC_VERSION_TAG,
+        PUBLIC_VERSION_TYPE
+    } from '$env/static/public';
     import Select from '$lib/components/ui/Select.svelte';
     import { ui } from '$lib/stores/ui.svelte';
 
@@ -8,6 +13,16 @@
         { value: 'light', label: 'Light' },
         { value: 'dark', label: 'Dark' }
     ];
+
+    const versionHref =
+        PUBLIC_VERSION_TYPE === 'release' || PUBLIC_VERSION_TYPE === 'prerelease'
+            ? `https://www.github.com/Kusefiru/Mist/releases/tag/${PUBLIC_VERSION_TAG}`
+            : `https://www.github.com/Kusefiru/Mist/commit/${PUBLIC_VERSION_COMMIT}`;
+    const versionLabel =
+        PUBLIC_VERSION_TYPE === 'release' || PUBLIC_VERSION_TYPE === 'prerelease'
+            ? PUBLIC_VERSION_TAG
+            : 'dev';
+    const commitLabel = PUBLIC_VERSION_COMMIT?.slice(0, 7);
 </script>
 
 <div class="overflow-auto p-6">
@@ -22,6 +37,28 @@
                 <h3>Theme</h3>
                 <div class="w-56">
                     <Select items={themeOptions} bind:value={ui.theme} />
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="mb-2 flex items-center gap-3">
+                <Info size="2rem" class="text-primary-10" />
+                <div>
+                    <h2 class="text-2xl font-semibold">About</h2>
+                </div>
+            </div>
+
+            <div class="flex items-center justify-between px-8 py-4">
+                <h3>Version</h3>
+
+                <div class="w-56 select-text">
+                    <a href={versionHref} target="_blank" class="text-primary-10 hover:underline"
+                        >{versionLabel}</a
+                    >
+                    {#if commitLabel}
+                        ({commitLabel})
+                    {/if}
                 </div>
             </div>
         </section>
